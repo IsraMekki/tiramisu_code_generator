24ea7244854bb53b7a94e41882d009f1ed50e856
isra <ei_mekki@esi.dz>
2019-03-18 15:22:53 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/restructured.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/isra/Desktop/restructured/classes.cpp" "/home/isra/Desktop/restructured/cmake-build-debug/CMakeFiles/restructured.dir/classes.cpp.o"
+  "/home/isra/Desktop/restructured/external_writers.cpp" "/home/isra/Desktop/restructured/cmake-build-debug/CMakeFiles/restructured.dir/external_writers.cpp.o"
+  "/home/isra/Desktop/restructured/main.cpp" "/home/isra/Desktop/restructured/cmake-build-debug/CMakeFiles/restructured.dir/main.cpp.o"
+  "/home/isra/Desktop/restructured/tiramisu_code_generator.cpp" "/home/isra/Desktop/restructured/cmake-build-debug/CMakeFiles/restructured.dir/tiramisu_code_generator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/restructured.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/restructured.dir/main.cpp.o"
+  "CMakeFiles/restructured.dir/classes.cpp.o"
+  "CMakeFiles/restructured.dir/tiramisu_code_generator.cpp.o"
+  "CMakeFiles/restructured.dir/external_writers.cpp.o"
+  "restructured.pdb"
+  "restructured"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/restructured.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
